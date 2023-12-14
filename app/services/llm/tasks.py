a817b37af14cd7907f3e734beec3c8b45142b95b
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2023-12-14 12:18:48 +0100

    Add named arguments to make it cleaner

--- app/services/llm/tasks.py
+++ app/services/llm/tasks.py
@@ -9,6 +9,15 @@ class Task:
     Base class for all tasks.
     """
 
+    # The system prompt that controls the behaviour of the model.
+    system_prompt: str
+
+    # The user prompt that is formatted with the named placeholders unless you override the format_user_prompt function.
+    user_prompt: str
+
+    # The functions that are used to extract the arguments from the model output, can be set to [] if not used.
+    functions: JSON
+
     def __init__(self, system_prompt: str, user_prompt: str, functions: JSON):
         self.system_prompt = system_prompt
         self.user_prompt = user_prompt
@@ -26,11 +35,11 @@ class Task:
 class RecommendPropertiesTask(Task):
     def __init__(self):
         super().__init__(
-            "You are an assistant for building a knowledge graph for science. "
+            system_prompt="You are an assistant for building a knowledge graph for science. "
             "Your task is to recommend additional related predicates based on "
             "the set of existing predicates. Recommend a list maximum 5 additional predicates.",
-            "The existing predicates are: {0}",
-            [
+            user_prompt="The existing predicates are: {0}",
+            functions=[
                 {
                     "name": "getProperties",
                     "description": "The array of additionally recommended properties",
@@ -62,11 +71,11 @@ class RecommendPropertiesTask(Task):
 class RecommendResearchProblemsTask(Task):
     def __init__(self):
         super().__init__(
-            "A research problem contains a maximum of approximately 4 words "
+            system_prompt="A research problem contains a maximum of approximately 4 words "
             "to explain the research task or topic of a paper. Provide a list of maximum 5 research problems "
             "based on the title and optionally abstract provided by the user.",
-            "{paperTitle} {abstract}",
-            [
+            user_prompt="{paperTitle} {abstract}",
+            functions=[
                 {
                     "name": "getResearchProblems",
                     "description": "Get a array of research problems",
@@ -91,11 +100,11 @@ class RecommendResearchProblemsTask(Task):
 class RecommendMethodsTask(Task):
     def __init__(self):
         super().__init__(
-            "Extract a list of maximum 5 methods from a scientific paper "
+            system_prompt="Extract a list of maximum 5 methods from a scientific paper "
             "based on the title and optionally abstract provided by the user. "
             "if no methods are found, return an empty array.",
-            "{paperTitle} {abstract}",
-            [
+            user_prompt="{paperTitle} {abstract}",
+            functions=[
                 {
                     "name": "getMethods",
                     "description": "Get a array of methods",
@@ -120,11 +129,11 @@ class RecommendMethodsTask(Task):
 class RecommendMaterialsTask(Task):
     def __init__(self):
         super().__init__(
-            "Extract a list of maximum 5 materials that are used in a scientific paper."
+            system_prompt="Extract a list of maximum 5 materials that are used in a scientific paper."
             "Extract it from the title and optionally abstract provided by the user. "
             "if no materials are found, return an empty array.",
-            "{paperTitle} {abstract}",
-            [
+            user_prompt="{paperTitle} {abstract}",
+            functions=[
                 {
                     "name": "getMaterials",
                     "description": "Get a array of materials",
@@ -149,11 +158,11 @@ class RecommendMaterialsTask(Task):
 class RecommendPropertiesFromTextTask(Task):
     def __init__(self):
         super().__init__(
-            "Act as an ORKG researcher. Return in JSON. Provide only property names "
+            system_prompt="Act as an ORKG researcher. Return in JSON. Provide only property names "
             "without values or extra information. Recommend a maximum of 3 to 4 properties for each text "
             "selection.",
-            "Find the best property names for the selected text: {selectedText}",
-            [
+            user_prompt="Find the best property names for the selected text: {selectedText}",
+            functions=[
                 {
                     "name": "getProperties",
                     "description": "The array of additionally recommended properties",
@@ -183,11 +192,11 @@ class RecommendPropertiesFromTextTask(Task):
 class CheckDescriptivenessTask(Task):
     def __init__(self):
         super().__init__(
-            "Provide feedback to a user on how to improve a provided description text. The "
+            system_prompt="Provide feedback to a user on how to improve a provided description text. The "
             "description text should give information about the objectives and topics of a scientific "
             "tabular related work overview. ",
-            "{value}",
-            [
+            user_prompt="{value}",
+            functions=[
                 {
                     "name": "getFeedback",
                     "description": "Get feedback for the provided description",
@@ -209,11 +218,11 @@ class CheckDescriptivenessTask(Task):
 class CheckResourceDestructuringTask(Task):
     def __init__(self):
         super().__init__(
-            "You are an assistant for building a knowledge graph for science. Provide advice on if "
+            system_prompt="You are an assistant for building a knowledge graph for science. Provide advice on if "
             "and how to decompose a provided resource label into separate resources. Only provide feedback is "
             "decomposing makes sense. Return the feedback in JSON.",
-            "{label}",
-            [
+            user_prompt="{label}",
+            functions=[
                 {
                     "name": "provideDecomposeFeedback",
                     "description": "Provide feedback on how a resource label can be decomposed",
@@ -236,12 +245,12 @@ class CheckResourceDestructuringTask(Task):
 class CheckIfLiteralTypeIsCorrectTask(Task):
     def __init__(self):
         super().__init__(
-            "You are an assistant in building a knowledge graph for science. You task is to advice "
+            system_prompt="You are an assistant in building a knowledge graph for science. You task is to advice "
             "users whether they should use a RDF resource or RDF literal. Based on a user-provided label, advice "
             "whether the type should be 'literal' or 'resource'. Literals are generally larger pieces of text and "
             "are not reusable, resource are atomic and can be reused. Return in JSON.",
-            "{label}",
-            [
+            user_prompt="{label}",
+            functions=[
                 {
                     "name": "getFeedback",
                     "description": "Provide feedback on whether the label should be a 'literal' or 'resource'",
@@ -264,13 +273,13 @@ class CheckIfLiteralTypeIsCorrectTask(Task):
 class CheckPropertyLabelGuidelinesTask(Task):
     def __init__(self):
         super().__init__(
-            "You are an assistant in building a knowledge graph for science. Provide feedback "
+            system_prompt="You are an assistant in building a knowledge graph for science. Provide feedback "
             "whether the provided predicate label is generic enough to make it reusable in the graph and explain "
             "how to make it more generic. Examples of properties that are not reusable: population in Berlin "
             "(because it contains a location), temperature in degrees Celsius (because it contains a unit). "
             "Return in JSON.",
-            "The label is: {label}",
-            [
+            user_prompt="The label is: {label}",
+            functions=[
                 {
                     "name": "getFeedback",
                     "description": "Determine whether the label is generic enough to make it reusable in the graph",
